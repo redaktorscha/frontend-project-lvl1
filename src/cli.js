12ed4ee4bc2264afda2ad3db1ad18527b2608af2
Masha <nasonkina@yandex.ru>
2021-11-17 23:34:54 +0300

    lint project

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,9 @@ import readlineSync from 'readline-sync';
  * ask user name and greet
  */
 const greetUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}`);
-}
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+};
 
 export default greetUser;
