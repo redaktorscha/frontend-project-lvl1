75ebfd27236572324c2fa96aae0cf4cee465aa8a
Masha <nasonkina@yandex.ru>
2021-11-28 16:06:31 +0300

    add brain-calc, fix Makefile

--- Makefile
+++ Makefile
@@ -6,3 +6,5 @@ brain-games:
 	node bin/brain-games.js
 publish:
 	npm publish --dry-run
+link:
+	npm link @hexlet/code@1.0.0
---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playGame from '../index.js';
+
+playGame('calc', 3); // game name, attempts count
