dd979f65d9d840926c9516dad60b5ee9e3dac4a2
Masha <nasonkina@yandex.ru>
2021-11-28 15:54:10 +0300

    fix answers, rules and tasks dicts to manage all game's types

--- src/gameRules.js
+++ src/gameRules.js
@@ -2,9 +2,17 @@
  * game rules (console messages) depending on game title
  * @typedef {Object} gameRules
  * @property {string} even brain-even game
+ * @property {string} calc brain-calc game
+ * @property {string} gcd brain-gcd game
+ * @property {string} prime brain-prime game
+ * @property {string} progression brain-progression game
  */
 const gameRules = {
   even: 'Answer \'yes\' if the number is even, otherwise answer \'no\'.',
+  calc: 'What is the result of the expression?',
+  gcd: 'Find the greatest common divisor of given numbers.',
+  prime: 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.',
+  progression: 'What number is missing in this progression?',
 };
 
 export default gameRules;
--- src/gameTasks.js
+++ src/gameTasks.js
@@ -1,10 +1,19 @@
+import operators from './math/operators.js';
 /**
  * game tasks (console messages) depending on game title
  * @typedef {Object} gameTasks
  * @property {function(Array): string)} even brain-even game
+ * @property {function(Array): string)} calc brain-calc game
+ * @property {function(Array): string)} gcd brain-gcd game
+ * @property {function(Array): string)} prime brain-prime game
+ * @property {function(Array): string)} progression brain-progression game
  */
 const gameTasks = {
   even: (input) => `Question: ${input[0]}.`,
+  calc: (input) => `Question: ${input[0]} ${operators[input[2]]} ${input[1]}.`,
+  gcd: (input) => `Question: ${input[0]} ${input[1]}.`,
+  prime: (input) => `Question: ${input[0]}.`,
+  progression: (input) => `Question: ${input.slice(0, -1).join(' ')}.`,
 };
 
 export default gameTasks;
