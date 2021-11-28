f835a613deccf15786d676aa4d2a7c700f51521a
Masha <nasonkina@yandex.ru>
2021-11-28 15:54:43 +0300

    fix getGameInputs to manage all game's types

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import playGame from '../src/playGame.js';
+import playGame from '../index.js';
 
-playGame('even', 3);
+playGame('even', 3); // game name, attempts count
--- src/getGameInputs.js
+++ src/getGameInputs.js
@@ -1,5 +1,6 @@
 import generateRandInt from './generateRandInt.js';
-
+import operators from './math/operators.js';
+import createSequence from './math/createSequence.js';
 /**
  * get numbers for the current task depending on the game title
  * @param {string} gameTitle name of the game
@@ -7,8 +8,22 @@ import generateRandInt from './generateRandInt.js';
  */
 const getGameInputs = (gameTitle) => {
   const inputs = [];
-  if (gameTitle === 'even') {
+  if (gameTitle === 'even' || gameTitle === 'prime') { // 1 random int
     inputs.push(generateRandInt());
+  } else if (gameTitle === 'gcd' || gameTitle === 'calc') { // 2 rand ints
+    for (let i = 0; i < 2; i += 1) {
+      inputs.push(generateRandInt());
+    }
+    if (gameTitle === 'calc') { // + math op
+      const opArr = Object.keys(operators);
+      const len = opArr.length;
+      const randomIdx = generateRandInt(len);
+      inputs.push(opArr[randomIdx]); // 'add', 'substr' or 'mult'
+    }
+  } else { // progression
+    const seqLen = 8; // could be any
+    const range = 10; // why not
+    inputs.push(...createSequence(seqLen, range));
   }
   return inputs;
 };
