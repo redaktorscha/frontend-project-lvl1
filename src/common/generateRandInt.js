77615e85e4f6b186408d4e9b6ed69845ba9313b8
Masha <nasonkina@yandex.ru>
2021-12-07 15:08:47 +0300

    fix structure add utils

--- src/common/generateRandInt.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {number} [len=100]
- * @returns {number}
- */
-const generateRandInt = (len = 100) => Math.floor(Math.random() * len);
-
-export default generateRandInt;
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number} [len=100]
+ * @returns {number}
+ */
+export const generateNumber = (len = 100) => Math.floor(Math.random() * len);
