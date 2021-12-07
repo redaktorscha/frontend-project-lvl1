7486875e6adf25a9b0ef7cd667844232a6354a6a
Masha <nasonkina@yandex.ru>
2021-12-07 15:12:20 +0300

    fix index

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ import readlineSync from 'readline-sync';
 const maxAttempts = 3;
 
 /**
- * @param {string} gameRule
+ * @param {string} description
  * @param {function(): Array<string>} getGameData
  */
-const playGame = (gameRule, getGameData) => {
+const runGame = (description, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(gameRule);
+  console.log(description);
 
-  let attemptCount = 0;
+  let attemptsCount = 0;
 
-  while (attemptCount < maxAttempts) {
-    const [currentTask, correctAnswer] = getGameData();
+  while (attemptsCount < maxAttempts) {
+    const [question, correctAnswer] = getGameData();
 
-    console.log(currentTask);
+    console.log(question);
 
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      attemptCount += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer :-( Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer :-( Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
+    console.log('Correct!');
+    attemptsCount += 1;
   }
   console.log(`Congratulations, ${userName}!`);
 };
 
-export default playGame;
+export default runGame;
