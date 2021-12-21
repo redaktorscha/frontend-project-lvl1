481f60a14d99f1670c8c28d05aff30824032f39c
Masha <nasonkina@yandex.ru>
2021-12-21 04:08:47 +0300

    fix progression game, namings and structure

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,4 +1,4 @@
-import { createSequence } from '../utils.js';
+import generateNumber from '../utils.js';
 import runGame from '../../index.js';
 
 /**
@@ -7,15 +7,50 @@ import runGame from '../../index.js';
 const description = 'What number is missing in this progression?';
 
 /**
+ * @param {number} length
+ * @param {number} interval
+ * @returns {Array<number>}
+ */
+const createSequence = (length, interval) => {
+  const sequence = [];
+
+  const gap = generateNumber(1, interval);
+  const start = generateNumber(0, interval);
+
+  let elem = start;
+  for (let i = 0; i < length; i += 1) {
+    sequence.push(elem);
+    elem += gap;
+  }
+
+  return sequence;
+};
+
+/**
+ *
+ * @param {Array<number>} arr
+ * @param {number} index
+ * @returns {Array<number|string>}
+ */
+const getQuestion = (arr, index) => arr.map((el, i) => {
+  if (i === index) {
+    return '..';
+  }
+  return el;
+});
+
+/**
  * @returns {Array<string>}
  */
 const makeGameData = () => {
   const sequenceLength = 10;
   const range = 10;
-  const randomSequence = createSequence(sequenceLength, range);
 
-  const answer = randomSequence.slice(-1).toString();
-  const question = `Question: ${randomSequence.slice(0, -1).join(' ')}.`;
+  const sequence = createSequence(sequenceLength, range);
+  const index = generateNumber(0, sequenceLength);
+
+  const answer = sequence[index].toString();
+  const question = `Question: ${getQuestion(sequence, index).join(' ')}.`;
   return [question, answer];
 };
 
